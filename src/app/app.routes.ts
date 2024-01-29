2a015c8a2fd02b4ceb53481e4ae965d01f22d475
KronosCodeur <piraliarris@gmail.com>
2024-01-29 22:29:03 +0000

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./security/login/login.component";
+import {RegisterComponent} from "./security/register/register.component";
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  }
+];
