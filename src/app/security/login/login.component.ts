9a943e8bf8ce13b6a48d9e77a9f8cf1e587b6c1d
KronosCodeur <piraliarris@gmail.com>
2024-01-30 15:39:24 +0000

    login component

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -1,6 +1,9 @@
 import {Component, inject} from '@angular/core';
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {ClientInterface} from "../client-interface";
+import {AuthService} from "../auth-service.service";
 
 @Component({
   selector: 'app-login',
@@ -14,11 +17,24 @@ import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 })
 export class LoginComponent {
   fb = inject(FormBuilder);
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  authService = inject(AuthService);
   form = this.fb.nonNullable.group({
-    email: ['',Validators.required],
-    password: ['',Validators.required]
+    email: ['', Validators.required],
+    password: ['', Validators.required]
   })
-  onSubmit():void {
-    console.log('login');
+  private apiUrl = 'http://localhost:8080/ega/api/v1/auth/authenticate';
+
+  onSubmit(): void {
+    this.http.post<ClientInterface>(this.apiUrl, this.form.getRawValue(),).subscribe(
+      (response) => {
+        console.log(response);
+        console.log('Helloo', response.token);
+        this.authService.currentClientSig.set(response);
+        this.router.navigateByUrl('/dashboard').then();
+      }
+    );
   }
 }
