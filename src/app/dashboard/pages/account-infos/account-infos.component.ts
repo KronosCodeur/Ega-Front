f25b0fd95cbf7cc9863c261a935072d82848a8dc
KronosCodeur <piraliarris@gmail.com>
2024-01-30 15:39:59 +0000

    account infos component

---
+++ src/app/dashboard/pages/account-infos/account-infos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-infos',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-infos.component.html',
+  styleUrl: './account-infos.component.css'
+})
+export class AccountInfosComponent {
+
+}
